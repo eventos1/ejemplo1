bfa6865dceda2c40437067ba7bdb75d81e4e2056
Mansour <theboom.kajas@gmail.com>
2024-09-23 13:36:39 +0200

    Initial Commit

---
+++ Repaso Examen/Utils/UIImageViewExtension.swift
@@ -0,0 +1,27 @@
+//
+//  UIImageViewExtension.swift
+//  Repaso Examen
+//
+//  Created by Mañanas on 23/9/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func loadFrom(url: URL) {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+    
+    func loadFrom(url: String) {
+        self.loadFrom(url: URL(string: url)!)
+    }
+}
